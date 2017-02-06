842e460a6cc436820c6bd1b996286184ac7a12ed
jfalkinburg <jeffrey.falkinburg@usafa.edu>
2017-02-06 11:39:11 -0700

    updating datapath component declaration in the lab 2 pack file.

--- lab/lab2/code/lab2_pack.vhdl
+++ lab/lab2/code/lab2_pack.vhdl
@@ -67,23 +67,26 @@ end component;
 
 component lab2_datapath
     Port ( clk : in  STD_LOGIC;
-           reset_n : in  STD_LOGIC;
-			  SDATA_IN : in STD_LOGIC;
-			  BIT_CLK : in STD_LOGIC;
-			  SYNC : out STD_LOGIC;
-			  SDATA_OUT : out STD_LOGIC;
-			  AC97_n_RESET : out STD_LOGIC;
-			  tmds : out  STD_LOGIC_VECTOR (3 downto 0);
-           tmdsb : out  STD_LOGIC_VECTOR (3 downto 0);
-			  sw: out std_logic_vector(2 downto 0);
-			  cw: in std_logic_vector (2 downto 0);
-			  btn: in	STD_LOGIC_VECTOR(4 downto 0);
-			  exWrAddr: in std_logic_vector(9 downto 0);
-			  exWen, exSel: in std_logic;
-			  Lbus_out, Rbus_out: out std_logic_vector(15 downto 0);
-			  exLbus, exRbus: in std_logic_vector(15 downto 0);
-			  flagQ: out std_logic_vector(7 downto 0);
-			  flagClear: in std_logic_vector(7 downto 0));
+	reset_n : in  STD_LOGIC;
+	ac_mclk : out STD_LOGIC;
+	ac_adc_sdata : in STD_LOGIC;
+	ac_dac_sdata : out STD_LOGIC;
+	ac_bclk : out STD_LOGIC;
+	ac_lrclk : out STD_LOGIC;
+	scl : inout STD_LOGIC;
+	sda : inout STD_LOGIC;	
+	tmds : out  STD_LOGIC_VECTOR (3 downto 0);
+	tmdsb : out  STD_LOGIC_VECTOR (3 downto 0);
+	sw: out std_logic_vector(2 downto 0);
+	cw: in std_logic_vector (2 downto 0);
+	btn: in	STD_LOGIC_VECTOR(4 downto 0);
+	exWrAddr: in std_logic_vector(9 downto 0);
+	exWen, exSel: in std_logic;
+	Lbus_out, Rbus_out: out std_logic_vector(15 downto 0);
+	exLbus, exRbus: in std_logic_vector(15 downto 0);
+	flagQ: out std_logic_vector(7 downto 0);
+	flagClear: in std_logic_vector(7 downto 0));
+
 end component;
 
 
